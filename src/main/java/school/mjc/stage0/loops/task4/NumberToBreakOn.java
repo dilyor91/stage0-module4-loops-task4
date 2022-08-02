38d6f112c761083357fadf4c8ba4b7b28bf9443f
dilyor91 <dilyor_05@mail.ru>
2022-08-02 14:36:47 +0500

    Task #4. Loop Keywords v2

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,16 +2,15 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
-        if(toBreakWith<numberToGoUntil) {
-            for(int i = 1; i <= toBreakWith; i++) {
-                System.out.println(i);
-            }
-        } else {
+        boolean br = toBreakWith > numberToGoUntil ? true:false;
+        if(br)
             System.out.println("iterating till the end");
-            for(int i = 1; i <= numberToGoUntil; i++) {
-                System.out.println(i);
+
+        for(int i = 1; i <= numberToGoUntil; i++) {
+            System.out.println(i);
+            if(i==toBreakWith){
+                break;
             }
         }
-
     }
 }
